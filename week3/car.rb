1e964dff9f7360e2945ee5344ae06e927b3ddea3
sudodoki <smd.deluzion@gmail.com>
2013-03-04 13:55:04 +0200

    method missing

--- week3/car.rb
+++ week3/car.rb
@@ -1,28 +1,29 @@
 class Car
-  # creating setter and getter-setter methods next 
-  (ATTRIBUTES_ALLOWED = %w(engine size turbo)).each do |attr| 
-    attr_writer attr
-    define_method attr do |value = nil|
-      if value.nil?
-        instance_variable_get("@#{attr}")
+  # responding to setter and getter-setter methods
+  ATTRIBUTES_ALLOWED = %w(engine size turbo)
+  def method_missing(method, *args)
+    if ATTRIBUTES_ALLOWED.include? method or ATTRIBUTES_ALLOWED.include? method.to_s.sub('=','')
+      if args.empty?
+        instance_variable_get("@#{method}")
       else
-        set attr => value
+        attribute = {method.to_s.sub('=', '').to_sym => args[0] }
+        self.set attribute  
       end
+    else
+      super
     end
   end
 
   def initialize attributes = {}, &block
     if block_given?
-      self.instance_eval &block
+      instance_eval &block
     else
-      self.set attributes
+      set attributes
     end  
   end
    
   def set attributes
-    attributes.each do |key, value|
-        send "#{key}=", value 
-    end
+     attributes.each {|key, value| instance_variable_set("@#{key}", value)}     
     rescue
       print "LOL U FAG U DID SOMETHING WRONG WITH #{attributes.inspect} \n"        
   end
