6eb6408ed216a3729efcea368895becdea17bb22
sudodoki <smd.deluzion@gmail.com>
2013-04-05 01:04:48 +0300

    nested forms for editing albums on the go

--- week8/jukebox/app/models/album.rb
+++ week8/jukebox/app/models/album.rb
@@ -1,5 +1,7 @@
 class Album < ActiveRecord::Base
-  attr_accessible :title, :year
-  has_many :songs
+  attr_accessible :title, :year, :songs_attributes # the last one for nested removal
+  has_many :songs, :dependent => :destroy
   belongs_to :artist
+  accepts_nested_attributes_for :songs, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
+
 end
